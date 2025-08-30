c0801c9e0c46bdf36f31a1081a5d57dbe1dc8649
cyanryann <209932180+cyanryann@users.noreply.github.com>
2025-08-30 00:55:06 -0400

    handling git error

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 const choices = ["rock", "paper", "scissors"];
 const button = document.querySelector("#pressme");
+const result = document.querySelector("#resulttext");
+const buttons = document.querySelectorAll("button");
+const score = document.querySelector("#scoreboard");
 let humanScore = 0;
 let computerScore = 0;
+let isWaiting = false;
+let gameCount = 0;
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*3)];
 }
-function getHumanChoice() {
-    let playerC = prompt("Enter your move (rock, paper, or scissors):");
-    return playerC.toLowerCase();
-}
-function playRound() {
-    let humanChoice = getHumanChoice();
+function playRound(str) {
+    let humanChoice = str.toLowerCase();
     let computerChoice = getComputerChoice();
-    console.log(humanChoice);
     if (humanChoice == "rock")
     {
         if (computerChoice == "rock")
         {
             console.log("Rock and rock! You tied!");
+            result.innerHTML = "The computer chose rock. You Tie"
         }
         else if (computerChoice == "paper")
         {
             console.log("Paper beats rock! You lost!");
             computerScore++;
+            result.innerHTML = "The computer chose paper. You Lose..."
         }
         else 
         {
             console.log("Rock beats scissors! You win!");
             humanScore++;
+            result.innerHTML = "The computer chose scissors. You Win!"
         }
     }
     else if (humanChoice == "paper")
@@ -36,15 +39,18 @@ function playRound() {
         {
             console.log("Paper beats rock! You win!");
             humanScore++;
+            result.innerHTML = "The computer chose rock. You Win!"
         }
         else if (computerChoice == "paper")
         {
             console.log("Paper and paper! You tied!");
+            result.innerHTML = "The computer chose paper. You Tie"
         }
         else 
         {
             console.log("Scissors beats paper! You lost!");
             computerScore++;
+            result.innerHTML = "The computer chose scissors. You Lose...";
         }
     }
     else
@@ -53,38 +59,62 @@ function playRound() {
         {
             console.log("Rock beats scissors! You lose!");
             computerScore++;
+            result.innerHTML = "The computer chose rock. You Lose...";
         }
         else if (computerChoice == "paper")
         {
             console.log("Scissors beats paper! You win!");
             humanScore++;
+            result.innerHTML = "The computer chose paper. You Win!";
         }
         else 
         {
             console.log("Scissors and scissors! You tied!");
+            result.innerHTML = "The computer chose scissors. You Tie";
         }
     }
+
+    score.innerHTML = "You " + humanScore + " - " + computerScore + " Computer";
+    gameCount++;
+    if (gameCount == 5)
+    {
+        nowWaiting = false;
+        gameCount = 0;
+        result.innerHTML = "Out of 5 games, you won " + humanScore + ". ";
+        buttons.forEach((button) => {
+            if (button.id != "pressme")
+            {
+                button.disabled = true;
+                console.log("disabling stuff");
+            }
+
+        })
+        button.disabled = false;
+    }
 }
-function playGame() {
+function nowWaiting() {
+    isWaiting = true;
+    buttons.forEach((button) => {
+        button.disabled=false;
+    })
+    button.disabled = true;
     humanScore = 0;
     computerScore = 0;
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    if (humanScore > computerScore)
-    {
-        console.log("You win overall!");
-    }
-    else
-    {
-        console.log("The computer wins overall!");
-    }
+    score.innerHTML = "You " + humanScore + " - " + computerScore + " Computer";
 }
-if (button)
-{
-    console.log("button added!");
-    button.addEventListener("click", playGame);
-}
-
+button.addEventListener("click", nowWaiting);
+buttons.forEach((button) => {
+    if (button.id != "pressme")
+    {
+        button.addEventListener("click", () => {
+        console.log("button works");
+        if (isWaiting)
+        {
+            playRound(button.id);
+        }
+        
+    })
+    button.disabled = true;
+    }
+    
+})
